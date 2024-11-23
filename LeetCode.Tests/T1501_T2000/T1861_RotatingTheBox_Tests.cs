649ca19dc6d53ae2c11262829e4e1b5759eefdce
Miko <unyperm@gmail.com>
2024-11-23 11:02:32 +0500

    T072, T1861

---
+++ LeetCode.Tests/T1501_T2000/T1861_RotatingTheBox_Tests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.T1501_T2000.T1861_RotatingTheBox;
+
+namespace LeetCode.Tests.T1501_T2000;
+
+public class T1861_RotatingTheBox_Tests
+{
+    [Fact]
+    public void Test01()
+    {
+        var taskClass = new T_RotatingTheBox();
+
+        var box = new char[1][] {
+            new char[] { '#', '.', '#' }
+        };
+
+        var result = taskClass.RotateTheBox(box);
+
+        var expected = new char[3][] {
+            new char[] { '.' },
+            new char[] { '#' },
+            new char[] { '#' }
+        };
+
+        Assert.Equal(expected, result);
+    }
+}
