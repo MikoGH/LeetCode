1c2bf0a5a5cfe438d12ab7c8f91882eb1de50bd8
Miko <unyperm@gmail.com>
2024-11-24 17:11:59 +0500

    T1975

---
+++ LeetCode/T1501_T2000/T1975_MaximumMatrixSum/T_MaximumMatrixSum.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.T1501_T2000.T1975_MaximumMatrixSum;
+
+public class T_MaximumMatrixSum
+{
+    public long MaxMatrixSum(int[][] matrix)
+    {
+        int countMinus = 0;
+        int minElement = (int)1e5;
+        long sum = 0;
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            for (int j = 0; j < matrix[i].Length; j++)
+            {
+                int abs = matrix[i][j] < 0 ? -matrix[i][j] : matrix[i][j];
+                sum += abs;
+                if (abs < minElement)
+                    minElement = abs;
+                if (matrix[i][j] < 0)
+                    countMinus++;
+            }
+        }
+
+        if (countMinus % 2 == 0)
+            return sum;
+
+        return sum - 2 * minElement;
+    }
+}
