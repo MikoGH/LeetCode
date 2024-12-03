928a16c3f8ece04732afceb8f3231db2f46e3da7
Miko <unyperm@gmail.com>
2024-12-03 23:19:37 +0500

    T0051, T0052, T2109

---
+++ LeetCode.Tests/T0001_T0500/T0051_NQueens_Tests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.T0001_T0500.T0051_NQueens;
+
+namespace LeetCode.Tests.T0001_T0500;
+
+public class T0051_NQueens_Tests
+{
+    [Fact]
+    public void Test01()
+    {
+        var taskClass = new T_NQueens();
+
+        var n = 4;
+
+        var result = taskClass.SolveNQueens(n);
+
+        var expected = new string[2][] {
+            new string[4] { "..Q.", "Q...", "...Q", ".Q.." },
+            new string[4] { ".Q..", "...Q", "Q...", "..Q." }
+        };
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ LeetCode/T2001_T2500/T2109_AddingSpacesToAString/T_AddingSpacesToAString.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace LeetCode.T2001_T2500.T2109_AddingSpacesToAString;
+
+public class T_AddingSpacesToAString
+{
+    public string AddSpaces(string s, int[] spaces)
+    {
+        StringBuilder stringBuilder = new StringBuilder(s);
+        for (int i = 0; i < spaces.Length; i++)
+        {
+            stringBuilder.Insert(spaces[i] + i, ' ');
+        }
+        return stringBuilder.ToString();
+    }
+}
