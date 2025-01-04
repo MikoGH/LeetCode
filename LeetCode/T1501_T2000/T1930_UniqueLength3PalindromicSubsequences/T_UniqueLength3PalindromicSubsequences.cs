7b6fbe4ebd8714b0b14f53b2793077f649e96892
Miko <unyperm@gmail.com>
2025-01-04 14:13:06 +0500

    T0050, T1930, T2270, T3405

---
+++ LeetCode/T1501_T2000/T1930_UniqueLength3PalindromicSubsequences/T_UniqueLength3PalindromicSubsequences.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.T1501_T2000.T1930_UniqueLength3PalindromicSubsequences;
+
+public class T_UniqueLength3PalindromicSubsequences
+{
+    public int CountPalindromicSubsequence(string s)
+    {
+        var firstPos = new int[26];
+        var lastPos = new int[26];
+        var counts = new int[s.Length + 1][];
+        counts[0] = new int[26];
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (firstPos[s[i] - 'a'] == 0)
+                firstPos[s[i] - 'a'] = i + 1;
+            lastPos[s[i] - 'a'] = i + 1;
+            counts[i + 1] = (int[])counts[i].Clone();
+            counts[i + 1][s[i] - 'a']++;
+        }
+
+        var result = 0;
+
+        for (int i = 0; i < 26; i++)
+        {
+            for (int j = 0; j < 26; j++)
+            {
+                if (lastPos[i] > 0 && counts[lastPos[i] - 1][j] > counts[firstPos[i]][j])
+                    result++;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/T2001_T2500/T2270_NumberOfWaysToSplitArray/T_NumberOfWaysToSplitArray.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.T2001_T2500.T2270_NumberOfWaysToSplitArray;
+
+public class T_NumberOfWaysToSplitArray
+{
+    public int WaysToSplitArray(int[] nums)
+    {
+        long sum = nums[0];
+        
+        for (int i = 1; i < nums.Length; i++)
+        {
+            sum += nums[i];
+        }
+
+        long current = 0;
+        var result = 0;
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            current += nums[i];
+            if (sum - current <= current)
+                result++;
+        }
+
+        return result;
+    }
+}
