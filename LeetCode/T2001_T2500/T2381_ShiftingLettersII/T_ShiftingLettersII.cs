3158273e2ef834427e80aa42fc82efa07848e514
Miko <unyperm@gmail.com>
2025-01-06 13:29:45 +0500

    T1769, T2381

---
+++ LeetCode/T2001_T2500/T2381_ShiftingLettersII/T_ShiftingLettersII.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.T2001_T2500.T2381_ShiftingLettersII;
+
+public class T_ShiftingLettersII
+{
+    public string ShiftingLetters(string s, int[][] shifts)
+    {
+        var shiftCounts = new int[s.Length + 1];
+
+        for (int i = 0; i < shifts.Length; i++)
+        {
+            if (shifts[i][2] == 0)
+            {
+                shiftCounts[shifts[i][0]]--;
+                shiftCounts[shifts[i][1] + 1]++;
+            }
+            else
+            {
+                shiftCounts[shifts[i][0]]++;
+                shiftCounts[shifts[i][1] + 1]--;
+            }
+        }
+
+        var shift = 0;
+        var chars = s.ToCharArray();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            shift += shiftCounts[i];
+            var charNum = (chars[i] - 'a' + shift) % 26;
+            if (charNum < 0)
+                charNum = 'z' - 'a' + 1 + charNum;
+            chars[i] = (char)(charNum + 'a');
+        }
+
+        return new string(chars);
+    }
+}
