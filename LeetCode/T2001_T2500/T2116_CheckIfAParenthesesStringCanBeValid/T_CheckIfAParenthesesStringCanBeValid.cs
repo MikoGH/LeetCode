b72cfc8866527fd1f066b7e13918083b4c64d0f2
Miko <unyperm@gmail.com>
2025-01-14 10:20:15 +0500

    T2116, T2657, T3223

---
+++ LeetCode/T2001_T2500/T2116_CheckIfAParenthesesStringCanBeValid/T_CheckIfAParenthesesStringCanBeValid.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.T2001_T2500.T2116_CheckIfAParenthesesStringCanBeValid;
+
+public class T_CheckIfAParenthesesStringCanBeValid
+{
+    public bool CanBeValid(string s, string locked)
+    {
+        if (s.Length % 2 == 1)
+            return false;
+
+        var count = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (locked[i] == '1' && s[i] == ')')
+                count--;
+            else
+                count++;
+
+            if (count < 0)
+                return false;
+        }
+
+        count = 0;
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (locked[i] == '1' && s[i] == '(')
+                count--;
+            else
+                count++;
+
+            if (count < 0)
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ LeetCode/T2501_T3000/T2657_FindThePrefixCommonArrayOfTwoArrays/T_FindThePrefixCommonArrayOfTwoArrays.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.T2501_T3000.T2657_FindThePrefixCommonArrayOfTwoArrays;
+
+public class T_FindThePrefixCommonArrayOfTwoArrays
+{
+    public int[] FindThePrefixCommonArray(int[] A, int[] B)
+    {
+        var nums = new bool[A.Length + 1];
+        var result = new int[A.Length];
+
+        var count = 0;
+        for (int i = 0; i < A.Length; i++)
+        {
+            if (nums[A[i]])
+                count++;
+            nums[A[i]] = true;
+            if (nums[B[i]])
+                count++;
+            nums[B[i]] = true;
+            result[i] = count;
+        }
+
+        return result;
+    }
+}
