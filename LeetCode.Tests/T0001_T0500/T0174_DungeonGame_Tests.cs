4f05c5f57fdea5ff336df3788077b8b4ba8a7127
Miko <unyperm@gmail.com>
2025-01-15 22:48:32 +0500

    T0174, T0154, T2429

---
+++ LeetCode.Tests/T0001_T0500/T0174_DungeonGame_Tests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.T0001_T0500.T0174_DungeonGame;
+
+namespace LeetCode.Tests.T0001_T0500;
+
+public class T0174_DungeonGame_Tests
+{
+    [Fact]
+    public void Test01()
+    {
+        var taskClass = new T_DungeonGame();
+
+        var dungeons = new int[3][] {
+            new int[] { 1, -3, 3 },
+            new int[] { 0, -2, 0 },
+            new int[] { -3, -3, -3 }
+        };
+
+        var result = taskClass.CalculateMinimumHP(dungeons);
+
+        var expected = 3;
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ LeetCode/T0001_T0500/T0174_DungeonGame/T_DungeonGame.cs
@@ -0,0 +1,59 @@
+﻿namespace LeetCode.T0001_T0500.T0174_DungeonGame;
+
+public class T_DungeonGame
+{
+    public int CalculateMinimumHP(int[][] dungeon)
+    {
+        var minHealths = new int[dungeon.Length][];
+        var healths = new int[dungeon.Length][];
+
+        for (int i = 0; i < dungeon.Length; i++)
+        {
+            minHealths[i] = new int[dungeon[i].Length];
+            healths[i] = new int[dungeon[i].Length];
+        }
+
+        healths[0][0] = dungeon[0][0];
+        minHealths[0][0] = healths[0][0];
+        for (int j = 1; j < dungeon[0].Length; j++)
+        {
+            healths[0][j] = healths[0][j - 1] + dungeon[0][j];
+            minHealths[0][j] = Math.Min(minHealths[0][j - 1], healths[0][j]);
+        }
+
+        for (int i = 1; i < dungeon.Length; i++)
+        {
+            healths[i][0] = healths[i - 1][0] + dungeon[i][0];
+            minHealths[i][0] = Math.Min(minHealths[i - 1][0], healths[i][0]);
+            for (int j = 1; j < dungeon[i].Length; j++)
+            {
+                var maxPreviousHealth = Math.Max(healths[i - 1][j], healths[i][j - 1]);
+                var maxPreviousMinHealth = Math.Max(minHealths[i - 1][j], minHealths[i][j - 1]);
+                healths[i][j] = maxPreviousHealth + dungeon[i][j];
+                minHealths[i][j] = Math.Min(maxPreviousMinHealth, healths[i][j]);
+
+                //if (minHealths[i - 1][j] > minHealths[i][j - 1])
+                //{
+                //    healths[i][j] = healths[i - 1][j] + dungeon[i][j];
+                //    minHealths[i][j] = Math.Min(minHealths[i - 1][j], healths[i][j]);
+                //}
+                //else if (minHealths[i - 1][j] < minHealths[i][j - 1])
+                //{
+                //    healths[i][j] = healths[i][j - 1] + dungeon[i][j];
+                //    minHealths[i][j] = Math.Min(minHealths[i][j - 1], healths[i][j]);
+                //}
+                //else
+                //{
+                //    var maxPreviousHealth = Math.Max(healths[i - 1][j], healths[i][j - 1]);
+                //    healths[i][j] = maxPreviousHealth + dungeon[i][j];
+                //    minHealths[i][j] = Math.Min(minHealths[i - 1][j], healths[i][j]);
+                //}
+            }
+        }
+
+        if (minHealths[^1][^1] >= 0)
+            return 1;
+
+        return -minHealths[^1][^1] + 1;
+    }
+}
