642bb65cafc9e12da7a6e75aa93b35f98892a2bc
Miko <unyperm@gmail.com>
2025-01-18 16:13:56 +0500

    T1368, T2425, T2683

---
+++ LeetCode.Tests/T1001_T1500/T1368_MinimumCostToMakeAtLeastOneValidPathInAGrid_Tests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.T1001_T1500.T1368_MinimumCostToMakeAtLeastOneValidPathInAGrid;
+
+namespace LeetCode.Tests.T1001_T1500;
+
+public class T1368_MinimumCostToMakeAtLeastOneValidPathInAGrid_Tests
+{
+    [Fact]
+    public void Test01()
+    {
+        var taskClass = new T_MinimumCostToMakeAtLeastOneValidPathInAGrid();
+
+        var grid = new int[][]
+        {
+            new int[] { 1, 1, 3 },
+            new int[] { 3, 2, 2 },
+            new int[] { 1, 1, 4 }
+        };
+
+        var result = taskClass.MinCost(grid);
+
+        var expected = 0;
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ LeetCode/T2501_T3000/T2683_NeighboringBitwiseXOR/T_NeighboringBitwiseXOR.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.T2501_T3000.T2683_NeighboringBitwiseXOR;
+
+public class T_NeighboringBitwiseXOR
+{
+    public bool DoesValidArrayExist(int[] derived)
+    {
+        var original = new int[derived.Length + 1];
+        original[0] = 0;
+        for (int i = 0; i < derived.Length; i++)
+        {
+            original[i + 1] = derived[i] ^ original[i];
+        }
+
+        var isZero = original[0] == original[^1];
+
+        original[0] = 1;
+        for (int i = 0; i < derived.Length; i++)
+        {
+            original[i + 1] = derived[i] ^ original[i];
+        }
+
+        var isOne = original[0] == original[^1];
+
+        return isZero || isOne;
+    }
+}
