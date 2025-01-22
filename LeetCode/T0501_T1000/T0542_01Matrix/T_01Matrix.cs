17231e9b172a5809da923cf6013cbf06adab4184
Miko <unyperm@gmail.com>
2025-01-22 08:39:18 +0500

    T0542, T1765, T2017, T2661

---
+++ LeetCode/T0501_T1000/T0542_01Matrix/T_01Matrix.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCode.T0501_T1000.T0542_01Matrix;
+
+public class T_01Matrix
+{
+    public int[][] UpdateMatrix(int[][] mat)
+    {
+        var queue = new Queue<(int Y, int X)>();
+        var visited = new bool[mat.Length][];
+
+        for (int i = 0; i < mat.Length; i++)
+        {
+            visited[i] = new bool[mat[i].Length];
+            for (int j = 0; j < mat[i].Length; j++)
+            {
+                if (mat[i][j] == 0)
+                {
+                    queue.Enqueue((i, j));
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        while (queue.Count > 0)
+        {
+            var cell = queue.Dequeue();
+            if (cell.Y - 1 >= 0 && !visited[cell.Y - 1][cell.X])
+            {
+                mat[cell.Y - 1][cell.X] = mat[cell.Y][cell.X] + 1;
+                visited[cell.Y - 1][cell.X] = true;
+                queue.Enqueue((cell.Y - 1, cell.X));
+            }
+            if (cell.Y + 1 < mat.Length && !visited[cell.Y + 1][cell.X])
+            {
+                mat[cell.Y + 1][cell.X] = mat[cell.Y][cell.X] + 1;
+                visited[cell.Y + 1][cell.X] = true;
+                queue.Enqueue((cell.Y + 1, cell.X));
+            }
+            if (cell.X - 1 >= 0 && !visited[cell.Y][cell.X - 1])
+            {
+                mat[cell.Y][cell.X - 1] = mat[cell.Y][cell.X] + 1;
+                visited[cell.Y][cell.X - 1] = true;
+                queue.Enqueue((cell.Y, cell.X - 1));
+            }
+            if (cell.X + 1 < mat[0].Length && !visited[cell.Y][cell.X + 1])
+            {
+                mat[cell.Y][cell.X + 1] = mat[cell.Y][cell.X] + 1;
+                visited[cell.Y][cell.X + 1] = true;
+                queue.Enqueue((cell.Y, cell.X + 1));
+            }
+        }
+
+        return mat;
+    }
+}
---
+++ LeetCode/T2001_T2500/T2017_GridGame/T_GridGame.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.T2001_T2500.T2017_GridGame;
+
+public class T_GridGame
+{
+    public long GridGame(int[][] grid)
+    {
+        long sum0 = grid[0][^1];
+        long sum1 = grid[1][^1];
+        long total1 = 0;
+
+        for (int i = grid[1].Length - 1; i >= 0; i--)
+        {
+            total1 += grid[1][i];
+        }
+
+        long result = total1 - grid[1][^1];
+        for (int i = grid[0].Length - 2; i >= 0; i--)
+        {
+            sum1 += grid[1][i];
+            result = Math.Min(Math.Max(sum0, total1 - sum1), result);
+            sum0 += grid[0][i];
+        }
+
+        return result;
+    }
+}
