023320734e65611b99a4c2f81599f82f7cccfe06
Miko <unyperm@gmail.com>
2025-01-28 20:04:59 +0500

    T2127, T1462, T2127, T2658

---
+++ LeetCode.Tests/T2001_T2500/T2127_MaximumEmployeesToBeInvitedToAMeeting_Tests.cs
@@ -0,0 +1,62 @@
+﻿using LeetCode.T2001_T2500.T2127_MaximumEmployeesToBeInvitedToAMeeting;
+
+namespace LeetCode.Tests.T2001_T2500;
+
+public class T2127_MaximumEmployeesToBeInvitedToAMeeting_Tests
+{
+    [Fact]
+    public void Test01()
+    {
+        var taskClass = new T_MaximumEmployeesToBeInvitedToAMeeting();
+
+        var favourite = new int[] { 2, 2, 1, 2 };
+
+        var result = taskClass.MaximumInvitations(favourite);
+
+        var expected = 3;
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Test02()
+    {
+        var taskClass = new T_MaximumEmployeesToBeInvitedToAMeeting();
+
+        var favourite = new int[] { 6, 4, 4, 5, 0, 3, 3 };
+
+        var result = taskClass.MaximumInvitations(favourite);
+
+        var expected = 6;
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Test03()
+    {
+        var taskClass = new T_MaximumEmployeesToBeInvitedToAMeeting();
+
+        var favourite = new int[] { 3, 0, 1, 4, 1 };
+
+        var result = taskClass.MaximumInvitations(favourite);
+
+        var expected = 4;
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Test04()
+    {
+        var taskClass = new T_MaximumEmployeesToBeInvitedToAMeeting();
+
+        var favourite = new int[] { 1, 0, 0, 2, 1, 4, 7, 8, 9, 6, 7, 10, 8 };
+
+        var result = taskClass.MaximumInvitations(favourite);
+
+        var expected = 6;
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ LeetCode/T1001_T1500/T1462_CourseScheduleIV/T_CourseScheduleIV.cs
@@ -0,0 +1,72 @@
+﻿namespace LeetCode.T1001_T1500.T1462_CourseScheduleIV;
+
+public class T_CourseScheduleIV
+{
+    public IList<bool> CheckIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries)
+    {
+        var adjacent = new Dictionary<int, List<int>>();
+
+        foreach (var edge in prerequisites)
+        {
+            if (!adjacent.ContainsKey(edge[0]))
+                adjacent.Add(edge[0], new List<int>());
+            adjacent[edge[0]].Add(edge[1]);
+        }
+
+        var result = new bool[queries.Length];
+
+        for (int i = 0; i < queries.Length; i++)
+        {
+            var visited = new bool[numCourses];
+            result[i] = IsPrerequisite(adjacent, visited, queries[i][0], queries[i][1]);
+        }
+
+        return result;
+    }
+
+    private bool IsPrerequisite(Dictionary<int, List<int>> adjacent, bool[] visited, int from, int to)
+    {
+        visited[from] = true;
+
+        if (from == to) return true;
+
+        var result = false;
+
+        var neighbors = adjacent.GetValueOrDefault(from);
+
+        if (neighbors is null) return false;
+
+        foreach (var neighbor in neighbors)
+        {
+            if (!visited[neighbor])
+            {
+                result |= IsPrerequisite(adjacent, visited, neighbor, to);
+            }
+        }
+
+        return result;
+    }
+        //var indirectPrerequisites = new HashSet<int>[numCourses];
+        //var prerequisitesDictionary = prerequisites.GroupBy(x => x[1], x => x[0]).ToDictionary(x => x.Key, x => x);
+        //var courses = prerequisites.Select(x => x[0]).ToHashSet();
+
+        //var queue = new Queue<int>();
+        //for (int i = 0; i < numCourses; i++)
+        //{
+        //    if (!courses.Contains(i))
+        //    {
+        //        queue.Enqueue(i);
+        //        indirectPrerequisites[i] = new HashSet<int>();
+        //    }
+        //}
+
+        //while (queue.Count > 0)
+        //{
+        //    var course = queue.Dequeue();
+
+        //    foreach (var prerequisiteCourse in prerequisitesDictionary[course])
+        //    {
+
+        //    }
+        //}
+}
