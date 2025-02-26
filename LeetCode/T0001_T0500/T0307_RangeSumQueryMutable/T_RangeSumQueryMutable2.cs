40b9dc20a6837b0c9cb886492e0fdb35a282fc99
Miko <unyperm@gmail.com>
2025-02-26 21:43:59 +0500

    T0307, T0889, T1115, T1524, T1749, T2467

---
+++ LeetCode/T0001_T0500/T0307_RangeSumQueryMutable/T_RangeSumQueryMutable2.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCode.T0001_T0500.T0307_RangeSumQueryMutable;
+
+public class T_RangeSumQueryMutable2
+{
+    public class NumArray
+    {
+        int[] _nums;
+        int[] _fenvic;
+
+        public NumArray(int[] nums)
+        {
+            _nums = new int[nums.Length];
+            _fenvic = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Update(i, nums[i]);
+            }
+        }
+
+        public void Update(int index, int val)
+        {
+            var diff = val - _nums[index];
+            _nums[index] = val;
+
+            while (index < _fenvic.Length)
+            {
+                _fenvic[index] += diff;
+                index = index | (index + 1);
+            }
+        }
+
+        public int SumRange(int left, int right)
+        {
+            return GetSum(right) - GetSum(left - 1);
+        }
+
+        private int GetSum(int index)
+        {
+            var sum = 0;
+
+            while (index >= 0)
+            {
+                sum += _fenvic[index];
+                index = (index & (index + 1)) - 1;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ LeetCode/T1001_T1500/T1115_PrintFooBarAlternately/T_PrintFooBarAlternately.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode.T1001_T1500.T1115_PrintFooBarAlternately;
+
+public class T_PrintFooBarAlternately
+{
+    public class FooBar
+    {
+        private int n;
+        private object locker;
+        private bool fooTurn = true;
+
+        public FooBar(int n)
+        {
+            this.n = n;
+            locker = new object();
+        }
+
+        public void Foo(Action printFoo)
+        {
+            lock (locker)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    printFoo();
+                    fooTurn = false;
+                    Monitor.Pulse(locker);
+                    if (i < n - 1)
+                        while (!fooTurn) Monitor.Wait(locker);
+                }
+            }
+        }
+
+        public void Bar(Action printBar)
+        {
+            lock (locker)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    while(fooTurn) Monitor.Wait(locker);
+                    printBar();
+                    fooTurn = true;
+                    Monitor.Pulse(locker);
+                }
+            }
+        }
+    }
+}
