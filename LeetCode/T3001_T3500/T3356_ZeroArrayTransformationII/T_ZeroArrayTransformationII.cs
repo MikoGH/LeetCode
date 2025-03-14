8a58c399c7e47a665d923b234fcaaf4845420aae
Miko <unyperm@gmail.com>
2025-03-14 22:36:09 +0500

    T2226, T3356

---
+++ LeetCode/T3001_T3500/T3356_ZeroArrayTransformationII/T_ZeroArrayTransformationII.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode.T3001_T3500.T3356_ZeroArrayTransformationII;
+
+public class T_ZeroArrayTransformationII
+{
+    public int MinZeroArray(int[] nums, int[][] queries)
+    {
+        var decreases = new int[nums.Length + 1];
+
+        int l = -1;
+        int r = queries.Length + 1;
+        while (l + 1 < r)
+        {
+            var s = (l + r) >> 1;
+            if (!Check(nums, queries, s, decreases))
+                l = s;
+            else
+                r = s;
+        }
+
+        if (r == queries.Length + 1)
+            return -1;
+
+        return r;
+    }
+
+    private bool Check(int[] nums, int[][] queries, int s, int[] decreases)
+    {
+        Array.Clear(decreases);
+
+        for (int i = 0; i < s; i++)
+        {
+            decreases[queries[i][0]] += queries[i][2];
+            decreases[queries[i][1] + 1] -= queries[i][2];
+        }
+
+        var decreaseBy = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            decreaseBy += decreases[i];
+            if (nums[i] - decreaseBy > 0)
+                return false;
+        }
+
+        return true;
+    }
+}
