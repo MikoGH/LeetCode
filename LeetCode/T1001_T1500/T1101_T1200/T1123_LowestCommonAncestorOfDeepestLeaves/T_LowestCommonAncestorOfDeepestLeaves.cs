bc11d2cb3392f7f6823021dcc7ce1fb88d88b629
Miko <unyperm@gmail.com>
2025-04-04 13:26:27 +0500

    T1123, T2873, T2874

---
+++ LeetCode/T1001_T1500/T1101_T1200/T1123_LowestCommonAncestorOfDeepestLeaves/T_LowestCommonAncestorOfDeepestLeaves.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode.T1001_T1500.T1101_T1200.T1123_LowestCommonAncestorOfDeepestLeaves;
+
+public class T_LowestCommonAncestorOfDeepestLeaves
+{
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    TreeNode _result;
+    int _maxDepth = 0;
+
+    public TreeNode LcaDeepestLeaves(TreeNode root)
+    {
+        _result = root;
+
+        Dfs(root, 0);
+
+        return _result;
+    }
+
+    private int Dfs(TreeNode node, int depth)
+    {
+        var leftDepth = 0;
+        if (node.left is not null)
+            leftDepth = Dfs(node.left, depth + 1);
+
+        var rightDepth = 0;
+        if (node.right is not null)
+            rightDepth = Dfs(node.right, depth + 1);
+
+        if (depth > _maxDepth)
+        {
+            _maxDepth = depth;
+            _result = node;
+        }
+        if (leftDepth == rightDepth && rightDepth == _maxDepth)
+            _result = node;
+
+        return Math.Max(Math.Max(leftDepth, rightDepth), depth);
+    }
+}
---
+++ LeetCode/T2501_T3000/T2801_T2900/T2873_MaximumValueOfAnOrderedTripletI/T_MaximumValueOfAnOrderedTripletI.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.T2501_T3000.T2801_T2900.T2873_MaximumValueOfAnOrderedTripletI;
+
+public class T_MaximumValueOfAnOrderedTripletI
+{
+    public long MaximumTripletValue(int[] nums)
+    {
+        long maxNum = Math.Max(nums[0], nums[1]);
+        long minNum = nums[1];
+        long maxDifference = nums[0] - nums[1];
+        long result = 0;
+
+        for (var i = 2; i < nums.Length; i++)
+        {
+            long value = maxDifference * nums[i];
+            if (value > result)
+                result = value;
+
+            if (nums[i] < minNum)
+            {
+                minNum = nums[i];
+                var difference = maxNum - minNum;
+                if (difference > maxDifference)
+                    maxDifference = difference;
+            }
+
+            if (nums[i] > maxNum)
+            {
+                maxNum = nums[i];
+                minNum = nums[i];
+            }
+        }
+
+        return result;
+    }
+}
